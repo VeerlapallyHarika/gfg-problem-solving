7fe026da231dbe7c5544acae59eedfe30f940721
VeerlapallyHarika <harikareddy7241@gmail.com>
2026-02-21 22:58:08 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Find H-Index/find-hindex.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+class Solution {
+    public int hIndex(int[] citations) {
+        Arrays.sort(citations); 
+        int n = citations.length;
+        int h = 0;
+        
+        for (int i = 0; i < n; i++) {
+            int papersWithAtLeastThisCite = n - i; 
+            if (citations[i] >= papersWithAtLeastThisCite) {
+                h = papersWithAtLeastThisCite;
+                break; 
+            }
+        }
+        
+        return h;
+    }
+}
