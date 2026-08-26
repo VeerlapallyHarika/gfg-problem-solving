3a265d3be016950f9ab2e0f47791ae356e44c445
VeerlapallyHarika <harikareddy7241@gmail.com>
2026-08-26 14:23:13 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Sorted Insert Position/sorted-insert-position.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int searchInsertK(int arr[], int k) {
+        // code here
+        int low=0;
+        int n=arr.length;
+        int high=n-1;
+        int ans=n;
+        while(low<=high){
+         int mid = low + (high - low) / 2;
+            if(arr[mid]>=k){
+                ans=mid;
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return ans;
+    }
+};
