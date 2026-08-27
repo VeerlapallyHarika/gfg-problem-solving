cc3c8b8e41664fcd382114f49a5689c95a041ada
VeerlapallyHarika <harikareddy7241@gmail.com>
2026-08-27 23:50:25 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Largest Rectangle with Column Swaps/largest-rectangle-with-column-swaps.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int maxArea(int[][] mat) {
+        int n = mat.length;
+        int m = mat[0].length;
+
+        int[] height = new int[m];
+        int maxArea = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            for (int j = 0; j < m; j++) {
+                if (mat[i][j] == 1) {
+                    height[j]++;
+                } else {
+                    height[j] = 0;
+                }
+            }
+
+            int[] sorted = height.clone();
+            Arrays.sort(sorted);
+
+            for (int j = 0; j < m; j++) {
+                maxArea = Math.max(maxArea, sorted[j] * (m - j));
+            }
+        }
+
+        return maxArea;
+    }
+}
